a32d8cb81470cce63e9de1ff7be976431f60064f
FarenLiyadi13 <farenliyadi_19@kharisma.ac.id>
2021-11-20 16:09:07 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Logo, IlSuccessOrder} from './assets';
+
+const App = () => {
+  return (
+    <View>
+      <Logo />
+      <IlSuccessOrder />
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
---
+++ src/assets/illustrations/index.js
@@ -0,0 +1,9 @@
+import Logo from './Logo.svg';
+import IlSuccessSignUp from './SuccessSignUp.svg';
+import IlSuccessOrder from './SuccessOrder.svg';
+import IlEmptyOrder from './EmptyOrder.svg';
+
+export {IlSuccessSignUp};
+export {Logo};
+export {IlSuccessOrder};
+export {IlEmptyOrder};
